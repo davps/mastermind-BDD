f803e83e6a9f41cf292b58b3be4a02bd07944350
davps <davidperez3010@gmail.com>
2012-09-22 16:47:06 -0400

    Working/green example of start feature

--- lib/mastermind/game.rb
+++ lib/mastermind/game.rb
@@ -1,4 +1,12 @@
 module Mastermind
   class Game
+    def initialize(messenger)
+      @messenger = messenger
+    end
+
+    def start
+      @messenger.print "Welcome to Mastermind!"
+      @messenger.print "Enter guess:"
+    end
   end
 end
